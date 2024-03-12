68087a5aeef98a70efcb5b9a95e2cc21c3838966
Roshan0412 <roshanjha8200@gmail.com>
2024-03-12 23:18:59 +0530

    feat : Price Anan;ysis and link changes in index page

--- weather.php
+++ weather.php
@@ -12,25 +12,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.js"></script>
-    <script> // Specify the API endpoint for user data
-      const apiUrl = 'https://vegetablemarketprice.com/api/dataapi/market/gujarat/daywisedata?date=2024-03-11';
-
-      // Make a GET request using the Fetch API
-      fetch(apiUrl)
-      .then(response => {
-          if (!response.ok) {
-          throw new Error('Network response was not ok');
-          }
-          return response.json();
-      })
-      .then(userData => {
-          // Process the retrieved user data
-          console.log('User Data:', userData);
-      })
-      .catch(error => {
-          console.error('Error:', error);
-      });
-    </script>
+    
   </head>
   <body>
       <?php 
