cc96956ef839f1bc035ecb6fff90cd18cf6c17ee
Himanshu-kartela <himanshupam63@gmail.com>
2024-03-30 08:38:09 +0530

    modi " "

--- MainEvent.php
+++ MainEvent.php
@@ -12,12 +12,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css" rel="stylesheet" />
-    
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css" rel="stylesheet" /> 
     <link href="css/my/ownstyles1.css" rel="stylesheet">
     <link rel="stylesheet" href="css/my/font.css"/>    
     <link href="css/my/style.css" rel="stylesheet">
-    
     <link href="res/images/logo.png" rel="icon">
     <title> Events </title>
 
