c22186edb73c3accf4c8548b801f64ff3c82e181
Roshan0412 <roshanjha8200@gmail.com>
2024-04-14 21:56:20 +0530

    modi :: some more

--- admin/farmer_product.php
+++ admin/farmer_product.php
@@ -1,28 +1,28 @@
 <?php 
-    session_start();
+session_start();
 
-    $db_server = "localhost";
-    $db_user = "root";
-    $db_pass = "";
-    $db_name = "agrozen";
-    $link = mysqli_connect($db_server,$db_user,$db_pass,$db_name);
-    if($link == false)
-    {
-        die("Error : Couldn't Connect ". mysqli_connect_error());
-    }
+$db_server = "localhost";
+$db_user = "root";
+$db_pass = "";
+$db_name = "agrozen";
+$link = mysqli_connect($db_server,$db_user,$db_pass,$db_name);
+if($link == false)
+{
+    die("Error : Couldn't Connect ". mysqli_connect_error());
+}
 
-    $admin_id = isset($_SESSION['admin_id']) ? $_SESSION['admin_id'] : null;
-    if(isset($admin_id))
-    {
-        $id = $_SESSION['admin_id'];
-        $sql = "SELECT * FROM admin WHERE admin_id = $id";
+$admin_id = isset($_SESSION['admin_id']) ? $_SESSION['admin_id'] : null;
+if(isset($admin_id))
+{
+    $id = $_SESSION['admin_id'];
+    $sql = "SELECT * FROM admin WHERE admin_id = $id";
 
-        $result =mysqli_query($link,$sql);
-        $row = mysqli_fetch_array($result);
-        
-        $sql1 = "SELECT * FROM farmer_add_prod";
-        
-        $result1 =mysqli_query($link,$sql1);
+    $result =mysqli_query($link,$sql);
+    $row = mysqli_fetch_array($result);
+    
+    $sql1 = "SELECT * FROM farmer_add_prod";
+    
+    $result1 =mysqli_query($link,$sql1);
 ?>
 
 <!DOCTYPE html>
@@ -105,85 +105,11 @@
                             <span><i class="fa fa-angle-right float-right"></i></span>
                         </a>
                     </li>
-                    <!--
-                    <li class="w-full h-full py-3 px-2 border-b border-light-border">
-                        <a href="forms.html"
-                           class="font-sans font-hairline hover:font-normal text-sm text-nav-item no-underline">
-                            <i class="fab fa-wpforms float-left mx-2"></i>
-                            Forms
-                            <span><i class="fa fa-angle-right float-right"></i></span>
-                        </a>
-                    </li>
-                    <li class="w-full h-full py-3 px-2 border-b border-light-border">
-                        <a href="buttons.html"
-                           class="font-sans font-hairline hover:font-normal text-sm text-nav-item no-underline">
-                            <i class="fas fa-grip-horizontal float-left mx-2"></i>
-                            Buttons
-                            <span><i class="fa fa-angle-right float-right"></i></span>
-                        </a>
-                    </li>
-                    <li class="w-full h-full py-3 px-2 border-b border-light-border">
-                        <a href="tables.html"
-                           class="font-sans font-hairline hover:font-normal text-sm text-nav-item no-underline">
-                            <i class="fas fa-table float-left mx-2"></i>
-                            Tables
-                            <span><i class="fa fa-angle-right float-right"></i></span>
-                        </a>
-                    </li>
-                    <li class="w-full h-full py-3 px-2 border-b border-light-border">
-                        <a href="ui.html"
-                           class="font-sans font-hairline hover:font-normal text-sm text-nav-item no-underline">
-                            <i class="fab fa-uikit float-left mx-2"></i>
-                            Ui components
-                            <span><i class="fa fa-angle-right float-right"></i></span>
-                        </a>
-                    </li>
-                    <li class="w-full h-full py-3 px-2 border-b border-300-border">
-                        <a href="modals.html" class="font-sans font-hairline hover:font-normal text-sm text-nav-item no-underline">
-                            <i class="fas fa-square-full float-left mx-2"></i>
-                            Modals
-                            <span><i class="fa fa-angle-right float-right"></i></span>
-                        </a>
-                    </li>
-                    <li class="w-full h-full py-3 px-2">
-                        <a href="#"
-                           class="font-sans font-hairline hover:font-normal text-sm text-nav-item no-underline">
-                            <i class="far fa-file float-left mx-2"></i>
-                            Pages
-                            <span><i class="fa fa-angle-down float-right"></i></span>
-                        </a>
-                        <ul class="list-reset -mx-2 bg-white-medium-dark">
-                            <li class="border-t mt-2 border-light-border w-full h-full px-2 py-3">
-                                <a href="login.html"
-                                   class="mx-4 font-sans font-hairline hover:font-normal text-sm text-nav-item no-underline">
-                                    Login Page
-                                    <span><i class="fa fa-angle-right float-right"></i></span>
-                                </a>
-                            </li>
-                            <li class="border-t border-light-border w-full h-full px-2 py-3">
-                                <a href="register.html"
-                                   class="mx-4 font-sans font-hairline hover:font-normal text-sm text-nav-item no-underline">
-                                    Register Page
-                                    <span><i class="fa fa-angle-right float-right"></i></span>
-                                </a>
-                            </li>
-                            <li class="border-t border-light-border w-full h-full px-2 py-3">
-                                <a href="404.html"
-                                   class="mx-4 font-sans font-hairline hover:font-normal text-sm text-nav-item no-underline">
-                                    404 Page
-                                    <span><i class="fa fa-angle-right float-right"></i></span>
-                                </a>
-                            </li>
-                        </ul>
-                    </li>
-                    -->
                 </ul>
-
             </aside>
             <!--/Sidebar-->
             <!--Main-->
             <main class="bg-white-300 flex-1 p-3 overflow-hidden">
-
                 <div class="flex flex-1  flex-col md:flex-row lg:flex-row mx-2">
                     <div class="mb-2 border-solid border-gray-300 rounded border shadow-sm w-full">
                         <div class="bg-gray-200 px-2 py-3 border-solid border-gray-200 border-b">
@@ -235,48 +161,14 @@
                         </div>
                     </div>
                 </div>
-
             </main>
             <!--/Main-->
         </div>
-        <!--Footer
-        <footer class="bg-[#4CAF50] text-white p-2 ">
-            <div class="bg-gray-300 text-center">
-                <p class="text-sm text-gray-900 sm:text-center dark:text-gray-900 my-0 leading-normal">
-                    © 2024 <a href="index.php" class="hover:underline">AgroZen™</a>. All Rights Reserved.
-                </p>
-            </div>
-        </footer>
-        /footer-->
-
     </div>
-
 </div>
 
-<!--
-<footer class="w-full fixed bottom-0 left-0" id="FOOTER">
-    <div id="FOOTER">
-        <div class="bg-[#4CAF50] text-white text-3xl font-medium py-2 px-2 flex justify-between">
-            <p class="mr-2">LETS GET CONNECTED</p>
-            <div class="footer-icon">
-                <a href="https://www.instagram.com/your_instagram_username"><i class="fab fa-instagram mr-2"></i></a>
-                <a href="https://www.linkedin.com/in/your_linkedin_profile"><i class="fab fa-linkedin mr-2"></i></a>
-                <a href="https://plus.google.com/your_google_plus_profile"><i class="fab fa-google-plus-square mr-2"></i></a>
-                <a href="https://www.facebook.com/your_facebook_profile"><i class="fab fa-facebook mr-2"></i></a>
-                <a href="https://twitter.com/your_twitter_profile"><i class="fab fa-twitter mr-2"></i></a>
-            </div>
-        </div>
-        <div class="bg-gray-300 text-center">
-            <p class="text-sm text-gray-900 sm:text-center dark:text-gray-900 my-0 leading-normal">
-                © 2024 <a href="indexal.html" class="hover:underline">AgroZen™</a>. All Rights Reserved.
-            </p>
-        </div>
-    </div>
-</footer>
--->
-
 <script>
-    function insertProductView(productId, productName, productPrice, productDescription, productQuantity, productCategory, mainImageName, extraImageNames) {
+    function insertProductView(productId, productName, productPrice, productDescription, productQuantity, productCategory, mainImageName) {
         // Prepare the data to send
         var data = "product_id=" + productId +
                    "&product_name=" + encodeURIComponent(productName) +
@@ -284,9 +176,8 @@
                    "&product_description=" + encodeURIComponent(productDescription) +
                    "&product_quantity=" + productQuantity +
                    "&product_category=" + encodeURIComponent(productCategory) +
-                   "&main_image_name=" + encodeURIComponent(mainImageName) +
-                   "&extra_image_names=" + encodeURIComponent(extraImageNames.join(',')); // Convert array to string
-        
+                   "&main_image_name=" + encodeURIComponent(mainImageName);
+
         // Send AJAX request to PHP script
         var xhr = new XMLHttpRequest();
         xhr.open("POST", "insert_product_view.php", true);
@@ -348,8 +239,8 @@
 </html>
 
 <?php 
-    }
-    else {
-        echo "<script>window.location.href = 'login.php';</script>";
-    }
+}
+else {
+    echo "<script>window.location.href = 'login.php';</script>";
+}
 ?>
--- admin/insert_product_view.php
+++ admin/insert_product_view.php
@@ -1,40 +1,38 @@
 <?php
-// Include the database configuration file
-include_once 'config.php';
+session_start();
 
-// Initialize error message variable
-$errorUpload = '';
-
-if(isset($_POST['product_id']) && isset($_POST['product_name']) && isset($_POST['product_price']) && isset($_POST['product_description']) && isset($_POST['product_quantity']) && isset($_POST['product_category']) && isset($_POST['main_image_name']) && isset($_POST['extra_image_names'])){ 
-    // Extract data from POST variables
-    $productId = $_POST['product_id'];
-    $productName = $_POST['product_name'];
-    $productDescription = $_POST['product_description'];
-    $productPrice = $_POST['product_price'];
-    $productCategory = $_POST['product_category'];
-    $productQuantity = $_POST['product_quantity'];
-    $mainImageName = $_POST['main_image_name'];
-    $extraImageNames = explode(',', $_POST['extra_image_names']); // Convert string to array
-
-    // Insert main product details into the database
-    $insertProductSQL = "INSERT INTO product_view (prod_id, prod_name, prod_price, prod_desc, prod_quant, prod_cat, main_img) VALUES ('$productId', '$productName', '$productPrice', '$productDescription', '$productQuantity', '$productCategory', '$mainImageName')";
-    $resultProduct = mysqli_query($connection, $insertProductSQL);
-
-    if($resultProduct) {
-        // Insert extra images into the database
-        foreach($extraImageNames as $extraImageName) {
-            $insertExtraImageSQL = "INSERT INTO images (id, file_name, uploaded_on) VALUES ('$productId', '$extraImageName', NOW())";
-            $resultExtraImage = mysqli_query($connection, $insertExtraImageSQL);
-            if(!$resultExtraImage) {
-                $errorUpload .= $extraImageName . ' | ';
-            }
-        }
-    } else {
-        // Handle error if main product insertion fails
-        $errorUpload .= 'Error inserting main product details: ' . mysqli_error($connection);
-    }
+$db_server = "localhost";
+$db_user = "root";
+$db_pass = "";
+$db_name = "agrozen";
+$link = mysqli_connect($db_server,$db_user,$db_pass,$db_name);
+if($link == false)
+{
+    die("Error : Couldn't Connect ". mysqli_connect_error());
 }
 
-// Output any errors
-echo $errorUpload;
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Retrieve data from POST request
+    $productId = $_POST['product_id'];
+    $productName = $_POST['product_name'];
+    $productPrice = $_POST['product_price'];
+    $productDescription = $_POST['product_description'];
+    $productQuantity = $_POST['product_quantity'];
+    $productCategory = $_POST['product_category'];
+    $mainImageName = $_POST['main_image_name'];
+    
+    // Insert data into product_view table
+    $sql = "INSERT INTO product_view (prod_id, prod_name, prod_price, prod_desc, prod_quant, prod_cat, main_img) 
+            VALUES ('$productId', '$productName', '$productPrice', '$productDescription', '$productQuantity', '$productCategory', '$mainImageName')";
+    
+    if (mysqli_query($link, $sql)) {
+        echo "Product accepted and added to view!";
+    } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($link);
+    }
+} else {
+    echo "Invalid request!";
+}
+
+mysqli_close($link);
 ?>
