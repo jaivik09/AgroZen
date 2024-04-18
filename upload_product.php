27f93f413ca5f7d246254daa55573cbabbfe7132
Himanshu-kartela <himanshupam63@gmail.com>
2024-04-18 22:29:11 +0530

    modi "sweet msg in upload product"

--- upload_product.php
+++ upload_product.php
@@ -110,7 +110,7 @@
 
       <div class="input-box">
           <label class="block mb-2 text-sm font-medium text-#3CBC00-900 dark:text-white">Product ID</label>
-          <input type="text" name="pid" class=" bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter product name" required />
+          <input type="text" name="pid" class=" bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter product id" required />
         </div>
 
         <div class="input-box">
