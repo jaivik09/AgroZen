b10ccfcf477cc3c9a11f99485edada8ebb9dfc95
Roshan0412 <roshanjha8200@gmail.com>
2024-05-16 01:00:20 +0530

    Feat :: user side order history table of that user

--- admin/prod_orders.php
+++ admin/prod_orders.php
@@ -236,7 +236,7 @@
 
                         <div class="rounded overflow-hidden shadow bg-white mx-2 w-full">
                             <div class="px-6 py-2 border-b border-light-grey">
-                                <div class="font-bold text-xl">Consumer Data Table</div>
+                                <div class="font-bold text-xl">Product Order History</div>
                             </div>
                             <div class="table-responsive">
                                 <table class="table text-grey-darkest">
