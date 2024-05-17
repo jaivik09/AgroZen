8bc33e62cdfff7ca46fd7443f0e76a134118e1da
Roshan0412 <roshanjha8200@gmail.com>
2024-05-17 15:02:06 +0530

    modi :: nothing

--- sample_Prod_cat.php
+++ sample_Prod_cat.php
@@ -188,7 +188,7 @@
 
 
 <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js'></script>
-<script src='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/js/bootstrap.min.js'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/js/bootstrap.min.js'></script> 
 
   <!-- <script type="text/javascript">
   $(document).ready(function() {
