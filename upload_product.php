d34216778e9120cec915ce2bb5eaf828c6a92c41
jaivik09 <starboy.28.02.2002@gmail.com>
2024-05-20 00:00:10 +0530

    modify :: update add product

--- upload_product.php
+++ upload_product.php
@@ -111,7 +111,7 @@
             require_once('unsetheader.html');
         }
     ?>
-    <div class="container">
+    <!-- <div class="container">
       <div class="add" align="center">Add Product</div>
       <form action="upload.php" method="post" enctype="multipart/form-data" class="form">
 
@@ -164,12 +164,91 @@
           <label class=" label block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="user_avatar">More Images</label>
             <input type="file" name="extra_images[]"  class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" aria-describedby="user_avatar_help"   multiple>
             </div>
-            <input type="hidden" name="farmer" class="farmer_name" value="<?php echo $row['Name']; ?>">
+            <input type="hidden" name="farmer" class="farmer_name" value="<?php //echo $row['Name']; ?>">
             
             <div class="sub_but">
             <input type="submit" name="submit" value="submit" class="sub">
             </div>   
       </form>
 </div>
+  </body>
+</html> -->
+
+<div class="mt-10 sm:mt-0 align-middle">
+  <div class="md:grid md:grid-cols-2 md:gap-6">
+   
+    <div class="mt-5 md:mt-0 md:col-span-2">
+    <form action="upload.php" method="post" enctype="multipart/form-data">
+        <div class="shadow overflow-hidden sm:rounded-md">
+          <div class="px-4 py-5 bg-white sm:p-6">
+            <div class="grid grid-cols-6 gap-6">
+              <div class="col-span-6 sm:col-span-3">
+                <label for="first_name" class="block text-sm font-medium text-gray-700">Product Id</label>
+                <input type="text" name="pid" id="first_name" autocomplete="given-name" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+              </div>
+
+              <div class="col-span-6 sm:col-span-3">
+                <label for="last_name" class="block text-sm font-medium text-gray-700">Product name</label>
+                <input type="text" name="pname" id="last_name" autocomplete="family-name" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+              </div>
+
+              <div class="col-span-6">
+                <!-- <label for="email_address" class="block text-sm font-medium text-gray-700">Product Description</label>
+                <input type="text" name="pdes" id="email_address" autocomplete="email" class="mt-1 p-6 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"> -->
+                
+                <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Product Description</label>
+                <textarea id="message" rows="4" name="pdes" class="block p-2 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 " ></textarea>
+
+              </div>
+
+              <!-- <div class="col-span-6 sm:col-span-6 lg:col-span-2">
+                <label for="city" class="block text-sm font-medium text-gray-700">City</label>
+                <input type="text" name="city" id="city" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+              </div> -->
+
+              <div class="col-span-6 sm:col-span-3 ">
+                <label for="state" class="block text-sm font-medium text-gray-700">Product Price</label>
+                <input type="text" name="pprice" id="state" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+              </div>
+
+              <div class="col-span-6 sm:col-span-3 lg:col-span-2">
+                <label for="postal_code" class="block text-sm font-medium text-gray-700">Product Quantity</label>
+                <input type="text" name="pquant" id="postal_code" autocomplete="postal-code" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+              </div>
+
+              <div class="col-span-6 sm:col-span-3">
+                <label for="country" class="block text-sm font-medium text-gray-700">Select Category</label>
+                <select id="country" name="pcategory" autocomplete="country" class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                <option selected>Choose a category</option>
+                <option value="crops">crops</option>
+                <option value="vegetables">vegetables</option>
+                <option value="fruits">fruits</option>
+                <option value="cereals">cereals</option>
+                <option value="Pulses">Pulses</option>
+                <option value="fertilizers">fertilizers</option>
+                <option value="pesticides">pesticides</option>
+                </select>
+              </div>
+
+              <div class="col-span-6">
+              <label class=" label block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="user_avatar">Main Image</label>
+            <input type="file" name="main_image" class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" aria-describedby="user_avatar_help" required>
+            </div>
+
+              
+            </div>
+          </div>
+          <input type="hidden" name="farmer" class="farmer_name" value="<?php echo $row['Name']; ?>">
+          <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
+            <button type="submit" name="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+              Add
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
+
   </body>
 </html>
